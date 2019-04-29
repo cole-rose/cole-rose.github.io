d4ed14fb156c73b4a58722166733999e2456aa11
Cole Rose <colerose@berkeley.edu>
2019-04-28 18:55:31 -0700

    updated changes

--- assets/script/slider.js
+++ assets/script/slider.js
@@ -1,12 +1,16 @@
 var slideIndex = 1;
 showDivs(slideIndex);
+var pageNum = 1;
 function plusDivs(n) {
 	showDivs(slideIndex += n);
+	pageNum += n;
+	
 }
 
 function showDivs(n) {
 	var i;
 	var x = document.getElementsByClassName("page");
+	if (n > x.length) {slideIndex = 1} 
 	if (n < 1) {slideIndex = x.length};
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = "none";
